c5e278f4e1ad039a08329659d5785379954813e6
liuyun <yun.liu01@hand-china.com>
2018-05-23 16:00:56 +0800

    liuyun

--- src/main/java/org/jboss/netty/bootstrap/ConnectionlessBootstrap.java
+++ src/main/java/org/jboss/netty/bootstrap/ConnectionlessBootstrap.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 package org.jboss.netty.bootstrap;
 
 import java.net.InetSocketAddress;
@@ -116,17 +101,8 @@ import org.jboss.netty.channel.Channels;
  */
 public class ConnectionlessBootstrap extends Bootstrap {
 
-    /**
-     * Creates a new instance with no {@link ChannelFactory} set.
-     * {@link #setFactory(ChannelFactory)} must be called before any I/O
-     * operation is requested.
-     */
-    public ConnectionlessBootstrap() {
-    }
-
-    /**
-     * Creates a new instance with the specified initial {@link ChannelFactory}.
-     */
+    public ConnectionlessBootstrap() {}
+    
     public ConnectionlessBootstrap(ChannelFactory channelFactory) {
         super(channelFactory);
     }
